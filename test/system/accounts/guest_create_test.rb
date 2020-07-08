7d00fc7ab54021878f63c6ed67bd6c0d81caf361
Richard <richard.wigley@gmail.com>
2020-07-08 15:08:59 +0100

    Devise create account

---
+++ test/system/accounts/guest_create_test.rb
@@ -0,0 +1,30 @@
+require "application_system_test_case"
+
+module Accounts
+  class GuestCreateTest < ApplicationSystemTestCase
+    test "can sign up" do
+      visit new_account_registration_url
+
+      assert_selector "h1", text: "Create account"
+      fill_in "Email", with: "guest@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+
+      assert_difference("Account.count", 1) do
+        click_on "Create your Bug Tracker account"
+      end
+
+      assert_selector "p", text: "You have signed up successfully."
+    end
+
+    test "can display errors" do
+      visit new_account_registration_url
+
+      assert_no_difference("Account.count") do
+        click_on "Create your Bug Tracker account"
+      end
+
+      assert_selector "h2", text: "errors prohibited this account from being saved"
+    end
+  end
+end
