2b86d6d014f44c0c6416b99c91951d272d5ada82
Richard <richard.wigley@gmail.com>
2020-07-09 21:01:11 +0100

    Authenticate project controller

---
+++ test/system/projects/authenticate_guest_test.rb
@@ -0,0 +1,11 @@
+require "application_system_test_case"
+
+module Projects
+  class AuthenticateGuestTest < ApplicationSystemTestCase
+    test "cannot visit project" do
+      visit projects_url
+
+      assert_current_path new_account_session_path
+    end
+  end
+end
--- test/system/projects_test.rb
+++ test/system/projects_test.rb
@@ -2,7 +2,9 @@ require "application_system_test_case"
 
 class ProjectsTest < ApplicationSystemTestCase
   test "visiting the index" do
+    sign_in create(:account)
     visit projects_url
+
     assert_selector "h1", text: "Projects"
   end
 end
