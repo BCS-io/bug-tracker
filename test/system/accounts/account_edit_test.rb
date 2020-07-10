c1a0a62ae1314d0115f7c87811fa61f88d7b4ea0
Richard <richard.wigley@gmail.com>
2020-07-10 12:47:27 +0100

    Devise edit account

---
+++ test/system/accounts/account_edit_test.rb
@@ -0,0 +1,33 @@
+require "application_system_test_case"
+
+module Accounts
+  class AccountEditTest < ApplicationSystemTestCase
+    def setup
+      @account = create(:account, email: "guest@example.com", password: "password")
+      sign_in @account
+    end
+
+    test "can update" do
+      visit edit_account_registration_url
+
+      assert_selector "h1", text: "Edit Account"
+      assert_selector :link, text: "Sign out", href: destroy_account_session_path
+      fill_in "Email", with: "account@example.com"
+      fill_in "Current password", with: "password"
+
+      click_on "Update account"
+
+      @account.reload
+      assert_selector "p", text: "Your account has been updated successfully."
+      assert_equal "account@example.com", @account.email
+    end
+
+    test "can display errors" do
+      visit edit_account_registration_url
+
+      click_on "Update account"
+
+      assert_selector "h2", text: "prohibited this account from being saved"
+    end
+  end
+end
