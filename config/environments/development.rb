a801732fd032dd10f6eab6161bd86e62bc8ab981
Richard <richard.wigley@gmail.com>
2020-07-13 15:48:06 +0100

    Add bullet to development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,14 @@ Rails.application.configure do
 
   config.generators.assets = false
   config.generators.helper = false
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+    Bullet.skip_html_injection = false
+  end
 end
