02b6cb6d6370d04a4b964804b3dad0f6f52023fc
Richard <richard.wigley@gmail.com>
2020-07-13 13:45:38 +0100

    Seeding project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Project.delete_all
+Account.delete_all
+
+MULTIPLIER = 10
+MAX_PROJECTS = 3 * MULTIPLIER
+MAX_DAYS_AGO = 100
+
+Account.create!([
+  {email: "admin@example.com", username: "admin", password: "password", password_confirmation: "password", role: 1},
+  {email: "rich@example.com", username: "Rich", password: "password", password_confirmation: "password"},
+  {email: "other@example.com", username: "Other", password: "password", password_confirmation: "password"}
+])
+
+MAX_PROJECTS.times do
+  account = Account.order(Arel.sql("RANDOM()")).first
+  project_created = rand(0...MAX_DAYS_AGO)
+  FactoryBot.create(:project, lead: account, created_at: project_created.days.ago)
+end
