160a626948d7dfe83bd7e8adec99e8aabc191248
Richard <richard.wigley@gmail.com>
2020-07-19 13:15:30 +0100

    Add import/resolover to eslintrc.js
     - it only helps so much still modules missing

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,20 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
   },
   rules: {
     'no-underscore-dangle': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.scss'],
+        moduleDirectory: ['node_modules', 'app/javascript'],
+      },
+    },
   },
 };
