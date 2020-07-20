344b5c2ee97e0ec224dad4489621e5a9d71b7441
Richard <richard.wigley@gmail.com>
2020-07-20 11:39:32 +0100

    Add optimism for inline error feedback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_action_cable_identifier
+
+  private
+
+  def set_action_cable_identifier
+    cookies.encrypted[:account_id] = current_account&.id
+  end
 end
