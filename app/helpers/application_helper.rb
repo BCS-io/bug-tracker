d4fc9f64d44267d6bb3dbde55a83eb3973299fe7
Richard <richard.wigley@gmail.com>
2020-09-18 14:09:47 +0100

    ^Add flash message partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
     title ||= name.underscore.humanize
     inline_svg_pack_tag(filename, aria: true, nocomment: true, title: title, class: styles)
   end
+
+  def tailwind_classes_for(flash_type)
+    {
+      notice: "bg-green-400 border-green-700 text-white",
+      alert: "bg-red-400 border-red-700 text-black"
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
 end
